1c935c393ae1716570a109373d7976606403fbdf
mugl <manuel.gonzalez@gft.com>
2020-10-20 13:33:56 +0200

    Feature: added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
+import { AppRouting } from './app.routes';
 import { SharedModule } from './shared/shared.module';
 import { ComponentsModule } from './components/components.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     ComponentsModule,
-    SharedModule
+    SharedModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { CounterComponent } from './components/counter/counter.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const APP_ROUTES: Routes = [
+    { path: 'login', component: LogInComponent },
+    { path: 'counter', component: CounterComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'login' }
+];
+
+export const AppRouting = RouterModule.forRoot(APP_ROUTES);
